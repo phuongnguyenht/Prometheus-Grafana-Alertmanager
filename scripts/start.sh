708b20226ebe61aa7bdfa61e5ab0fd24e6bad0f9
Your Name <you@example.com>
2018-11-28 12:39:10 +0700

    delete start stop

--- scripts/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Start Prometheus + Grafana + AlertManager"
-cd /opt/script/
-
-nohup sh alertmanager.sh &
-nohup sh prometheus.sh &
-nohup sh grafana.sh &
-nohup sh exporter.sh &
-echo "Nolove"
